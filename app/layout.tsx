6b7d62e46bf06fb278b5024f682537e4701607ad
unknown <kavellid63@gmail.com>
2024-07-21 22:19:38 -0300

    styles importm, metadata object, main funtion export jsx corrections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import "@/styles/globals.css"
 import { Inter as FontSans } from "next/font/google"
+import './globals.css'
 
 import { cn } from "@/lib/utils"
 
@@ -8,17 +8,21 @@ const fontSans = FontSans({
   variable: "--font-sans",
 })
 
+export const metadata = {
+  title: 'LiveDocs',
+  description: ' Your collaborative editor'
+}
+
 export default function RootLayout({ children }: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   )
